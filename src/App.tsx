313bfbff7c3bc6ca7f3c252e0416a07aeff9324c
Elevane <pma.bastien.aubry@gmail.com>
2023-01-22 18:37:19 +0100

    feat : .env delete , style rm

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import { Routes, Route, Router } from "@solidjs/router"
-import { Toaster } from "solid-toast"
-import CreateAccount from "./Components/Authentification/CreateAccount"
-import Login from "./Components/Authentification/Login"
-import NotFound from "./Components/Authorization/NotFound"
-import ProtectedRoute from "./Components/Authorization/ProtectedRoute"
-import Home from "./Components/Home"
+import { Routes, Route, Router } from "@solidjs/router";
+import { Toaster } from "solid-toast";
+import CreateAccount from "./Components/Authentification/CreateAccount";
+import Login from "./Components/Authentification/Login";
+import NotFound from "./Components/Authorization/NotFound";
+import ProtectedRoute from "./Components/Authorization/ProtectedRoute";
+import Home from "./Components/Home";
 
 function App() {
-  const width = window.innerWidth
-  const height = window.innerHeight
-  return (<div style={{
-    "width" : `${width}px`,
-    "height" : `${height}px`,
-    'display' :"flex",
-    "flex-direction" : "column",
-    "align-items" :"center"
-  }}> 
-  <Toaster toastOptions={{ style : {"background" : "#A3B18A"}}} />
-   <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/createAccount" element={<CreateAccount />} />
-        <Route path="/*all" element={<NotFound />} />
-      </Routes>
-    </Router>
-  </div>
-   
-  )
+  return (
+    <div
+      style={{
+        display: "flex",
+        "flex-direction": "column",
+        "align-items": "center",
+      }}
+    >
+      <Toaster />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/createAccount" element={<CreateAccount />} />
+          <Route path="/*all" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Authentification/Login.jsx
+++ src/Components/Authentification/Login.jsx
@@ -1,71 +1,89 @@
 import { FaceOutlined } from "@suid/icons-material";
-import { Box, Avatar, Typography, TextField, Link, Grid, Button, Icon } from "@suid/material"
-import { createSignal } from "solid-js"
-import Spinner from 'solidjs-material-spinner';
+import {
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Link,
+  Grid,
+  Button,
+  Icon,
+} from "@suid/material";
+import { createSignal } from "solid-js";
+import Spinner from "solidjs-material-spinner";
 import UseApi from "../../Hooks/UseApi";
 
 export default function Login() {
-  const [email, setEmail] = createSignal("")
-  const [password, setPassword] = createSignal("")
-  const [loading, setLoading] = createSignal(false)
-
+  const [email, setEmail] = createSignal("");
+  const [password, setPassword] = createSignal("");
+  const [loading, setLoading] = createSignal(false);
 
   const handleSubmit = async (e) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
-    var res = await UseApi.login(email(), password())
-    console.log(res)
-  }
+    var res = await UseApi.login(email(), password());
+    setLoading(false);
+  };
 
   return (
-    <Box >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.mainz' }}>
-           <Icon><FaceOutlined></FaceOutlined></Icon>
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box style={{width : '400px'}} component="form" onSubmit={(e) => handleSubmit(e)} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              autoComplete="current-password"
-            />
-            <Button
-            disabled={loading()}
-              type="submit"
-              fullWidth
-              class="button"
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              { loading() ? <Spinner radius="25" stroke="3" color="#fff"/> : "Sign in"}
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-  )
+    <Box>
+      <Avatar sx={{ m: 1, bgcolor: "primary.mainz" }}>
+        <Icon>
+          <FaceOutlined></FaceOutlined>
+        </Icon>
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box
+        style={{ width: "400px" }}
+        component="form"
+        onSubmit={(e) => handleSubmit(e)}
+        noValidate
+        sx={{ mt: 1 }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+        />
+        <Button
+          disabled={loading()}
+          type="submit"
+          fullWidth
+          class="button"
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          {loading() ? (
+            <Spinner radius="25" stroke="3" color="#fff" />
+          ) : (
+            "Sign in"
+          )}
+        </Button>
+        <Grid container>
+          <Grid item>
+            <Link variant="body2">{"Don't have an account? Sign Up"}</Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 }
